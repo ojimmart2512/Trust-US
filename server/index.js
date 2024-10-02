10b6886f2bee763f4cebc11ec4a43afeb6ed2232
Omar Jimenez Martinez <ojimen412@west-mec.org>
2024-10-02 13:32:10 -0700

    updated public html pages

---
+++ server/index.js
@@ -0,0 +1,16 @@
+// Required Modules
+const express = require('express');
+const path = require('path');
+const fs = require('fs').promises;
+
+// Initialize Express application
+const app = express();
+
+// Define paths
+const clientPath = path.join(__dirname, '..', 'client/src');
+const dataPath = path.join(__dirname, 'data', 'users.json');
+const serverPublic = path.join(__dirname, 'public');
+// Middleware setup
+app.use(express.static(clientPath)); // Serve static files from client directory
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
+app.use(express.json()); // Parse JSON bodies
